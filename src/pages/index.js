04b34048c74ca529b65e0e3a7e447dd4d8e10c61
Chukwukaelo Osuizugbe <42609625+mail2chuka@users.noreply.github.com>
2024-03-28 17:25:22 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -40,13 +40,24 @@ export default function Home() {
         <Layout title={"Home Page"}>
         <ReviewSlider/>
         <Info/>
-        <div className="h-40 cursor-pointer bg-green-500">Winners for the Week</div>
+        <div className="h-80">
+           <p className="grid w-full  mx-auto text-center bg-gray-800 text-2xl text-gray-300 font-sans">
+              <span className="flex text-center text-2xl font-serif font-semibold mx-auto p-8">
+                Winners for the Week  
+              </span>
+            </p>
+            <ol className="h-80 pl-8">
+              <li>List</li>
+              <li>of</li>
+              <li>Winners</li>
+            </ol>
+         </div>
         <div className=""> 
             <p className="grid w-full  mx-auto text-center bg-gray-800 text-2xl text-gray-300 font-sans">
               <span className="flex text-center text-2xl font-serif font-semibold mx-auto p-8">
-                 <Image src={flameimg} height={10} width={30} alt="hot"/> 
+                 <Image quality={10} src={flameimg} height={10} width={30} alt="hot"/> 
                   Our Amazing Products
-                 <Image src={flameimg} height={10} width={30} alt="hot"/>
+                 <Image quality={10} src={flameimg} height={10} width={30} alt="hot"/>
               </span>
             </p>
         <div className=" mt-6 grid left-0 grid-cols-2 gap-4  md:grid-cols-2 lg:grid-cols-3 md:gap-4 mx-auto items-center md:max-w-screen-xl px-4 sm:px-3 lg:px-20">

1172f2d9cd5dbce11fa25f0d1cb2ce8eabc87509
Chukwukaelo Osuizugbe <42609625+mail2chuka@users.noreply.github.com>
2024-05-07 15:51:47 +0100

    Update reg.js

--- src/pages/reg.js
+++ src/pages/reg.js
@@ -127,6 +127,26 @@ Cookies.set(
           />
         </div>
         <div className="mb-4">
+          <input type="text" id="day-input" placeholder="Day (DD)" pattern="\d{1,2}" maxlength="2" />
+  <select id="month-select">
+    <option value="">--Select Month--</option>
+    <option value="01">January</option>
+    <option value="02">February</option>
+    <option value="03">March</option>
+    <option value="04">April</option>
+    <option value="05">May</option>
+    <option value="06">June</option>
+    <option value="07">July</option>
+    <option value="08">August</option>
+    <option value="09">September</option>
+    <option value="10">October</option>
+    <option value="11">November</option>
+    <option value="12">December</option>
+  </select>
+  <input type="text" id="year-input" placeholder="Year (YYYY)" pattern="\d{4}" maxlength="4" />
+  <input type="date" id="date-picker" />
+        </div>
+        <div className="mb-4">
           <label htmlFor="dob" className="block mb-2">Date of Birth</label>
           <input
             type="date"
